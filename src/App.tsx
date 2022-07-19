604c372f1f159c821e3487d21e03d08e758061c4
lviekmuf <danylo.sh@radency.com>
2022-07-19 15:39:40 +0300

    Added Room provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
+import RoomsInfo from './components/RoomsInfo';
+import RoomProvider from './context/RoomsContext';
 
 function App() {
   return (
-    <>
+    <RoomProvider>
       <h1 className="text-3xl font-bold underline text-red-600">
         Smart Host
       </h1>
-    </>
+    </RoomProvider>
   );
 }
 
