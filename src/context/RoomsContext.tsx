fe23576f5981fb3eec195704f5a8cfde76f61476
lviekmuf <danylo.sh@radency.com>
2022-07-19 18:47:14 +0300

    Added moked guests and added type to Room Context

--- src/context/RoomsContext.tsx
+++ src/context/RoomsContext.tsx
@@ -1,29 +1,22 @@
-import { createContext, useReducer } from "react";
-import { roomReducer, RoomsInterface } from "./reducers";
-
-
+import { createContext, useReducer, Dispatch } from "react";
+import { roomReducer, RoomsActions, RoomsInterface } from "./reducers";
+import guests from "../data/guests.json"
 const initialState: RoomsInterface = {
     usageRooms: {
         premium: 0,
         economy: 0
     },
-    guests: [
-        23,
-        45,
-        155,
-        374,
-        22,
-        99,
-        100,
-        101,
-        115,
-        209
-    ]
+    minPremiumPrice: 100,
+    guests
 }
-export const RoomContext = createContext<any | null>(null)
+export interface ContextInterface { state: RoomsInterface; dispatch: Dispatch<RoomsActions>; }
 
+export const RoomContext = createContext<ContextInterface>({
+    state: initialState,
+    dispatch: () => null
+})
 
-export const RoomProvider:React.FC = ({ children }: any) => {
+export const RoomProvider: React.FC = ({ children }: any) => {
     const [state, dispatch] = useReducer(roomReducer, initialState)
     const value = { state, dispatch }
     return (
